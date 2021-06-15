f8d4d5f14a6b561292df60d834432ca3160106a9
Stella Jin <stellajin@Stellas-MacBook-Air.local>
2021-06-15 19:39:59 +0200

    create context

--- src/store/favourites-context.js
+++ src/store/favourites-context.js
@@ -1,15 +1,45 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavouritesContext = createContext({
   favourites: [],
   totalFavourites: 0,
+  addFavourites: (favouriteMeetup) => {},
+  removeFavourites: (meetupId) => {},
+  itemIsFavourite: (meetupId) => {},
 });
 
-function FavouritesContextProvider(props) {
-  const context = {};
+export function FavouritesContextProvider(props) {
+  const [userFavourites, setUserFavourites] = useState([]);
+
+  function addFavourites(favouriteMeetup) {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.concat(favouriteMeetup);
+    });
+  }
+  function removeFavourites(meetupId) {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+  function itemIsFavourite(meetupId) {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.some((meetup) => meetup.id === meetupId);
+    });
+  }
+
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+    addFavourites: addFavourites,
+    removeFavourites: removeFavourites,
+    itemIsFavourite: itemIsFavourite,
+  };
+
   return (
     <FavouritesContext.Provider value={context}>
       {props.children}
     </FavouritesContext.Provider>
   );
 }
+
+export default FavouritesContext;
